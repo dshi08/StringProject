77066b4c87f522b8b2af87d028c80b57b66ff7e6
lfugere2026 <134296480+lfugere2026@users.noreply.github.com>
2025-01-10 11:08:46 -0500

    Add files via upload

--- Cookie.java
+++ Cookie.java
@@ -4,25 +4,192 @@
  * Cookie Class
  */
 
- import java.net.URI;
- import java.net.URL;
- import java.net.URLConnection;
- import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
 
- public class Cookie {
+public class Cookie {
     private String name = "";
     private double rating;
     private String review = "", description = "";
     public static final String CRUMBL_PAGE = "https://crumblcookies.com";
-
-    public Cookie(int day, int month, int year, int num) {
+    public Cookie(int day, int month, int year, int num)
+    {
         // TO-DO:
-        // 1. Make a file with all the weeks of crumbl and then scrape it looking for what the cookies are with the given date
+        // 1. Make a file with all the weeks of crumbl and then scrape it looking for what the
+        // cookies are with the given date
         // 2. Set cookie information depending on cookie number (maybe go based on rating)
         name = null;
         rating = 0.0;
         review = null;
         description = null;
+        String link2 = "";
+
+        String inputLine;
+
+        String line = "";
+        try
+        {
+            URL crumbl = new URI(CRUMBL_PAGE).toURL();
+            URLConnection cc = crumbl.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+
+                cc.getInputStream()));
+            while ((inputLine = in.readLine()) != null)
+
+                line += inputLine;
+
+            in.close();
+            int findSecondLink = 0;
+            String finder =
+                "rounded-full py-3 transition-all duration-300 ease-in group-hover:lg:pl-[30px] "
+                + "pl-[0px] pr-[30px] group-hover:lg:border border-transparent";
+            for (int i = 0; i < num; i++)
+            {
+                findSecondLink =
+                    line.indexOf("rounded-full py-3 transition-all duration-300 ease-in " +
+                                 "group-hover:lg:pl-[30px] "
+                                 + "pl-[0px] pr-[30px] group-hover:lg:border border-transparent");
+                if (i > 0)
+                {
+                    line = line.substring(findSecondLink + finder.length());
+                    findSecondLink = line.indexOf(
+                        "rounded-full py-3 transition-all duration-300 ease-in " +
+                        "group-hover:lg:pl-[30px] "
+                        + "pl-[0px] pr-[30px] group-hover:lg:border border-transparent");
+                }
+                line = line.substring(findSecondLink);
+            }
+            int findSecondLink1 = line.indexOf("https");
+            link2 = line.substring(findSecondLink1);
+            int findSecondLink2 = link2.indexOf("\"");
+            link2 = link2.substring(0, findSecondLink2);
+        } catch (FileNotFoundException ex)
+
+        {
+
+            System.out.println("unknown");
+
+        }
+
+        catch (MalformedURLException e)
+
+        {
+
+            System.out.println("badly formed url exception occurred");
+
+            return;
+
+        }
+
+        catch (URISyntaxException e)
+        {
+            System.out.println("Bad URI syntax");
+
+            return;
+
+        }
+
+        catch (IOException e)
+
+        {
+
+            System.out.println("IO exception occurred");
+
+            return;
+        }
+        try
+        {
+
+            URL cookie = new URI(link2).toURL();
+            URLConnection c = cookie.openConnection();
+
+            BufferedReader in2 = new BufferedReader(new InputStreamReader(
+
+                c.getInputStream()));
+
+            String inputLine2;
+
+            String line2 = "";
+
+            while ((inputLine2 = in2.readLine()) != null)
+
+                line2 += inputLine2;
+
+            in2.close();
+            int cookieName1 = line2.indexOf("alt=") + 5;
+            int cookieName2 = line2.indexOf("loading=") - 2;
+            name = line2.substring(cookieName1, cookieName2);
+            int ampChecker = name.indexOf("&amp");
+            if (ampChecker != -1)
+            {
+                name = name.substring(0, ampChecker) + " and" +
+                       name.substring(ampChecker + 4, name.length());
+            }
+            int rating1 = line2.indexOf("text-nowrap\">");
+            String textLocater = "text-nowrap\">";
+            String ratingS = line2.substring(rating1 + textLocater.length());
+            int rating2 = ratingS.indexOf(" ");
+            ratingS = ratingS.substring(0, rating2);
+            rating = (double) (Double.parseDouble(ratingS));
+            String descriptionLocater = "2xl:leading-[22px] md:max-w-[700px] text-xl leading-6\">";
+            int description1 =
+                line2.indexOf("2xl:leading-[22px] md:max-w-[700px] text-xl leading-6\">");
+            String descript = line2.substring(description1 + descriptionLocater.length());
+            int description2 = descript.indexOf(".");
+            description = descript.substring(0, description2 + 1);
+            if (description.length() > 200)
+            {
+                description = "N/A";
+            }
+            String topRLocater="ext-ellipsis line-clamp-5\">";
+            int topR1=line2.indexOf(topRLocater);
+            String topR=line2.substring(topR1+topRLocater.length());
+            int topR2=topR.indexOf("<");
+            review=topR.substring(0,topR2);
+
+        } catch (FileNotFoundException ex)
+
+        {
+
+            System.out.println("unknown");
+
+        }
+
+        catch (MalformedURLException e)
+
+        {
+
+            System.out.println("badly formed url exception occurred");
+
+            return;
+
+        }
+
+        catch (URISyntaxException e)
+        {
+            System.out.println("Bad URI syntax");
+
+            return;
+
+        }
+
+        catch (IOException e)
+
+        {
+
+            System.out.println("IO exception occurred");
+
+            return;
+        }
     }
 
     public String getName()
@@ -40,9 +207,9 @@
         return review;
     }
 
-
     public String toString()
     {
-        return "Name: " + name + "\nRating: " + rating + "\nDescription: " + description + "\nTop Review: " + review;
+        return "Name: " + name + "\nRating: " + rating + "\nDescription: " + description +
+            "\nTop Review: " + review;
     }
 }
