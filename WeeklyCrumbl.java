08b443846c94a4c40c9c8bad40432d90bcbcfee1
David S <116216182+dshi08@users.noreply.github.com>
2025-01-10 23:10:15 -0500

    Update WeeklyCrumbl.java

--- WeeklyCrumbl.java
+++ WeeklyCrumbl.java
@@ -20,64 +20,137 @@ public class WeeklyCrumbl {
     private Cookie cookie7;
     private Cookie cookie8;
 
-    public WeeklyCrumbl(int day, int month, int year)
+    public WeeklyCrumbl(int month, int day, int year)
     {
         this.day = day;
         this.month = month;
         this.year = year;
-        cookie1 = new Cookie(day, month, year, 1);
-        cookie2 = new Cookie(day, month, year, 2);
-        cookie3 = new Cookie(day, month, year, 3);
-        cookie4 = new Cookie(day, month, year, 4);
-        cookie5 = new Cookie(day, month, year, 5);
-        cookie6 = new Cookie(day, month, year, 6);
-        cookie7 = new Cookie(day, month, year, 7);
-        cookie8 = new Cookie(day, month, year, 8);
+
+        cookie1 = new Cookie(1);
+        cookie2 = new Cookie(2);
+        cookie3 = new Cookie(3);
+        cookie4 = new Cookie(4);
+        cookie5 = new Cookie(5);
+        cookie6 = new Cookie(6);
+        cookie7 = new Cookie(7);
+        cookie8 = new Cookie(8);
+
+        sort();
     }
 
-    public Cookie getCookie1() {
-        return cookie1;
+    public void sort()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 1; j <= 8 - i - 1; j++)
+            {
+                Cookie current = getCookie(j);
+                Cookie next = getCookie(j + 1);
+
+                if (current.getRating() - next.getRating() < 0.0001)
+                {
+                    setCookie(j, next);
+                    setCookie(j + 1, current);
+                }
+            }
+        }
     }
 
-    public Cookie getCookie2() {
-        return cookie2;
+    public Cookie getCookie(int index)
+    {
+        switch (index)
+        {
+            case 1:
+                return cookie1;
+            case 2:
+                return cookie2;
+            case 3:
+                return cookie3;
+            case 4:
+                return cookie4;
+            case 5:
+                return cookie5;
+            case 6:
+                return cookie6;
+            case 7:
+                return cookie7;
+            case 8:
+                return cookie8;
+            default:
+                throw new IllegalArgumentException("Invalid cookie index: " + index);
+        }
     }
 
-    public Cookie getCookie3() {
-        return cookie3;
-    }
-
-    public Cookie getCookie4() {
-        return cookie4;
-    }
-
-    public Cookie getCookie5() {
-        return cookie5;
-    }
-
-    public Cookie getCookie6() {
-        return cookie6;
-    }
-
-    public Cookie getCookie7() {
-        return cookie7;
-    }
-
-    public Cookie getCookie8() {
-        return cookie8;
+    private void setCookie(int index, Cookie cookie)
+    {
+        switch (index)
+        {
+            case 1:
+                cookie1 = cookie;
+                break;
+            case 2:
+                cookie2 = cookie;
+                break;
+            case 3:
+                cookie3 = cookie;
+                break;
+            case 4:
+                cookie4 = cookie;
+                break;
+            case 5:
+                cookie5 = cookie;
+                break;
+            case 6:
+                cookie6 = cookie;
+                break;
+            case 7:
+                cookie7 = cookie;
+                break;
+            case 8:
+                cookie8 = cookie;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid cookie index: " + index);
+        }
     }
 
     public String toString()
     {
-        return "Cookies of the week " + month + "/" + day + "/" + year +
-            ":\n-------------------------------------------------------------\n" + cookie1 +
-            "\n-------------------------------------------------------------\n" + cookie2 +
-            "\n-------------------------------------------------------------\n" + cookie3 +
-            "\n-------------------------------------------------------------\n" + cookie4 +
-            "\n-------------------------------------------------------------\n" + cookie5 +
-            "\n-------------------------------------------------------------\n" + cookie6 +
-            "\n-------------------------------------------------------------\n" + cookie7 +
-            "\n-------------------------------------------------------------\n" + cookie8 +
-            "\n-------------------------------------------------------------\n";
+        return "Top cookies of the week " + month + "/" + day + "/" + year +
+            (":\n--------------------------------------------------------------------------------"
+             + "-----------------------------------------------------------------------------------"
+             + "--------------------\n") +
+            cookie1 +
+            ("\n---------------------------------------------------------------------------------"
+             + "-----------------------------------------------------------------------------------"
+             + "-------------------\n") +
+            cookie2 +
+            ("\n---------------------------------------------------------------------------------"
+             + "-----------------------------------------------------------------------------------"
+             + "-------------------\n") +
+            cookie3 +
+            ("\n---------------------------------------------------------------------------------"
+             + "-----------------------------------------------------------------------------------"
+             + "-------------------\n") +
+            cookie4 +
+            ("\n---------------------------------------------------------------------------------"
+             + "-----------------------------------------------------------------------------------"
+             + "-------------------\n") +
+            cookie5 +
+            ("\n---------------------------------------------------------------------------------"
+             + "-----------------------------------------------------------------------------------"
+             + "-------------------\n") +
+            cookie6 +
+            ("\n---------------------------------------------------------------------------------"
+             + "-----------------------------------------------------------------------------------"
+             + "-------------------\n") +
+            cookie7 +
+            ("\n---------------------------------------------------------------------------------"
+             + "-----------------------------------------------------------------------------------"
+             + "-------------------\n") +
+            cookie8 +
+            ("\n---------------------------------------------------------------------------------"
+             + "-----------------------------------------------------------------------------------"
+             + "-------------------\n");
     }
 }
